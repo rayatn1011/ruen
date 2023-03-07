f1d61f425bc8bccc6a2298284919b2aff9fabb7d
RomanChen <stubstub8257@gmail.com>
2023-03-08 00:02:31 +0800

    feat: add interface

--- src/features/apis/useChat.ts
+++ src/features/apis/useChat.ts
@@ -1,5 +1,28 @@
 import { useState } from 'react'
 import { openAiApiUrl } from '../utils'
+interface ChatData {
+  role: string
+  content: string
+}
+interface ChatCompletion {
+  id: string
+  object: string
+  created: number
+  model: string
+  usage: {
+    prompt_tokens: number
+    completion_tokens: number
+    total_tokens: number
+  }
+  choices: {
+    message: {
+      role: string
+      content: string
+    }
+    finish_reason: string
+    index: number
+  }[]
+}
 
 const useChat = () => {
   const [fetchState, setFetchState] = useState({
