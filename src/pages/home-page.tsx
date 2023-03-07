4ecea17db2ae90a5541ed0c335be90024c842f3e
ChunRen Zhang <rayatn1011@gmail.com>
2023-03-07 08:57:19 +0800

    feat: chage style in home-page

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -5,27 +5,25 @@ function HomePage() {
   const [outputText, setOutputText] = useState('')
 
   return (
-    <div className="h-screen w-screen bg-stone-100 p-16">
-      <div className="flex h-full w-full gap-8">
-        <div className="relative flex-1 shrink-0">
-          <textarea
-            className="h-full w-full resize-none"
-            autoComplete="false"
-            placeholder="Please enter text."
-            onChange={(e) => setInputText(e.target.value)}
-          >
-            {inputText}
-          </textarea>
-        </div>
-        <div className="flex-1 shrink-0 border">
-          <textarea
-            readOnly
-            className="h-full w-full resize-none"
-            autoComplete="false"
-          >
-            {outputText}
-          </textarea>
-        </div>
+    <div className="flex h-full w-full gap-8">
+      <div className="relative flex-1 shrink-0">
+        <textarea
+          className="h-full w-full resize-none"
+          autoComplete="false"
+          placeholder="Please enter text."
+          onChange={(e) => setInputText(e.target.value)}
+        >
+          {inputText}
+        </textarea>
+      </div>
+      <div className="flex-1 shrink-0 border">
+        <textarea
+          readOnly
+          className="h-full w-full resize-none"
+          autoComplete="false"
+        >
+          {outputText}
+        </textarea>
       </div>
     </div>
   )
