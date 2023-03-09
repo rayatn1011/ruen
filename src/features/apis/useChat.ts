dd72fe5c3edfe2c9543296746018ca94edeca750
Roman <60188137+rccj@users.noreply.github.com>
2023-03-09 10:25:02 +0800

    fix: add generics for fetchState

--- src/features/apis/useChat.ts
+++ src/features/apis/useChat.ts
@@ -1,105 +1,109 @@
-import { useState, useRef, useEffect } from 'react'
-import { openAiApiUrl } from '../utils'
-import { getThirdPartyApi } from '../utils/third-party-api'
+import { useState, useRef, useEffect } from "react";
+import { openAiApiUrl } from "../utils";
+import { getThirdPartyApi } from "../utils/third-party-api";
 
 interface ChatData {
-  role: string
-  content: string
+  role: string;
+  content: string;
 }
 interface ChatCompletion {
-  id: string
-  object: string
-  created: number
-  model: string
+  id: string;
+  object: string;
+  created: number;
+  model: string;
   usage: {
-    prompt_tokens: number
-    completion_tokens: number
-    total_tokens: number
-  }
+    prompt_tokens: number;
+    completion_tokens: number;
+    total_tokens: number;
+  };
   choices: {
     message: {
-      role: string
-      content: string
-    }
-    finish_reason: string
-    index: number
-  }[]
+      role: string;
+      content: string;
+    };
+    finish_reason: string;
+    index: number;
+  }[];
 }
 
 const useChat = () => {
-  const apiKey = getThirdPartyApi()?.openAi
-  const fetchController = useRef<AbortController | null>(null)
-  const [fetchState, setFetchState] = useState({
-    data: {} as ChatCompletion,
+  const apiKey = getThirdPartyApi()?.openAi;
+  const fetchController = useRef<AbortController | null>(null);
+  const [fetchState, setFetchState] = useState<{
+    data: ChatCompletion | null;
+    isLoading: boolean;
+    isError: boolean;
+  }>({
+    data: null,
     isLoading: false,
     isError: false,
-  })
+  });
 
   const fetchApi = (chatData: ChatData[]) => {
     if (fetchController.current !== null) {
-      fetchController.current.abort()
+      fetchController.current.abort();
     }
 
-    fetchController.current = new AbortController()
+    fetchController.current = new AbortController();
 
     setFetchState((prev) => ({
       ...prev,
       isLoading: true,
       isError: false,
-    }))
-    fetch(openAiApiUrl + 'v1/chat/completions', {
-      method: 'POST',
+    }));
+    fetch(openAiApiUrl + "v1/chat/completions", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authorization: `Bearer ${apiKey}`,
       },
       body: JSON.stringify({
-        model: 'gpt-3.5-turbo',
+        model: "gpt-3.5-turbo",
         messages: chatData,
       }),
       signal: fetchController.current.signal,
     })
       .then((response) => {
         if (!response.ok) {
-          throw response
+          throw response;
         }
-        return response.json()
+        return response.json();
       })
       .then((responseData) => {
         setFetchState((prev) => ({
           ...prev,
           data: responseData,
-        }))
+        }));
       })
       .catch((error) => {
-        console.error(error)
+        console.error(error);
         setFetchState((prev) => ({
           ...prev,
           isError: true,
-        }))
+        }));
       })
       .finally(() => {
         setFetchState((prev) => ({
           ...prev,
           isLoading: false,
-        }))
-      })
-  }
+        }));
+      });
+  };
 
   useEffect(() => {
     return () => {
       if (fetchController.current !== null) {
-        fetchController.current.abort()
+        fetchController.current.abort();
       }
-    }
-  }, [])
+    };
+  }, []);
 
   return {
     chatData: fetchState.data,
     isChatLoading: fetchState.isLoading,
     isChateError: fetchState.isError,
     fetchChat: fetchApi,
-  }
-}
+  };
+};
 
-export { useChat }
+export { useChat };
