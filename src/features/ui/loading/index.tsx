0f343ae8af504a34152b4489a526879190555fb8
ChunRen Zhang <rayatn1011@gmail.com>
2023-03-16 00:57:54 +0800

    feat: re-style loading

--- src/features/ui/loading/index.tsx
+++ src/features/ui/loading/index.tsx
@@ -2,10 +2,10 @@ import IconLoading from '~icons/material-symbols/app-badging-outline'
 
 function Loading() {
   return (
-    <div className="absolute top-0 right-0 bottom-0 left-0 z-50 flex items-center justify-center bg-stone-900/90">
+    <div className="absolute top-0 right-0 bottom-0 left-0 z-50 flex items-center justify-center bg-stone-50/90">
       <div className="flex cursor-wait select-none flex-col items-center justify-center gap-1 motion-safe:animate-bounce">
-        <IconLoading className="block animate-spin text-2xl text-teal-500" />
-        <div className="text-sm text-stone-300">Loading</div>
+        <IconLoading className="block animate-spin text-2xl text-teal-600" />
+        <div className="text-sm text-teal-600">Loading</div>
       </div>
     </div>
   )
